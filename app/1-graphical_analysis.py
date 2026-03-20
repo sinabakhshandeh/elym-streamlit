85b7950d39aa8c1cc3a31265d6fd67b733f6b8fa
sinabakhshandeh <sina.bakhshandeh.t@gmail.com>
2026-03-20 10:47:02 +0100

    feat: add states name inside bar - remove legend of chart

--- app/1-graphical_analysis.py
+++ app/1-graphical_analysis.py
@@ -125,10 +125,15 @@ df_sorted = df.sort_values("Number of announced projects", ascending=True)
 
 st.subheader("Project Distribution")
 
-bar_chart = pygal.HorizontalBar(style=pygal.style.TurquoiseStyle)
+bar_chart = pygal.HorizontalBar(
+    show_legend=False, print_labels=True, style=pygal.style.LightGreenStyle
+)
 bar_chart.title = "Project Distribution by State"
 for _, row in df_sorted.iterrows():
-    bar_chart.add(row["State name"], row["Number of announced projects"])
+    bar_chart.add(
+        row["State name"],
+        [{"value": row["Number of announced projects"], "label": row["State name"]}],
+    )
 bar_chart.render()
 
 rendered_bar_chart = bar_chart.render().decode("utf-8")
